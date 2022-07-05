be4b2781c1d03267d9e97a475b44672f0cd28ba6
huanglei1 <huanglei1@joyy.com>
2022-07-05 14:13:27 +0800

    反转部分区间

--- src/main/java/List/ReverseList.java
+++ src/main/java/List/ReverseList.java
@@ -31,5 +31,59 @@ public class ReverseList {
         ListUtils.printList(head);
     }
 
+    /**
+     * LeetCode92 ：给你单链表的头指针 head 和两个整数 left 和 right ，其中 left <= right。请你反转从位
+     * 置 left 到位置 right 的链表节点，返回反转后的列表
+     * 方法1：部分区间先反转，然后重新连接
+     * @param head
+     * @param left
+     * @param right
+     * @return
+     */
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode p = head;
+        int index = 0;
+        ListNode pre = null;
+        ListNode preLeft = null;
+        ListNode rightNext = null;
+        ListNode leftNode = null;
+
+        //确定左右节点
+        while (p != null) {
+            if (index == left) {
+                leftNode = p;
+                preLeft = pre;
+            }
+            if (index == right) {
+                rightNext = p.next;
+            }
+            pre = p;
+            p = p.next;
+            index++;
+        }
+        //反转列表
+        ListNode cur = leftNode;
+        pre = null;
+        while (cur != rightNext) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+
+        // 重新连接左右两个节点
+        preLeft.next = pre;
+        leftNode.next = rightNext;
+        return head;
+    }
+
+
+    @Test
+    public void testReverseBetween() {
+        int[] a = {1, 2, 3, 4, 5, 6, 7, 8};
+        ListNode head = reverseBetween(ListUtils.arrayToList(a), 2, 5);
+        ListUtils.printList(head);
+    }
+
 
 }
