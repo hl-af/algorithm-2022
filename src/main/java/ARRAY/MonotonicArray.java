ee5eea3b8acdd2ac68cdcd4ec8c3b88b8a069598
huanglei1 <huanglei1@joyy.com>
2022-07-17 12:37:48 +0800

    合并两个有序数组

--- src/main/java/ARRAY/MonotonicArray.java
+++ src/main/java/ARRAY/MonotonicArray.java
@@ -2,6 +2,7 @@ package ARRAY;
 
 import org.junit.Test;
 
+import javax.management.remote.rmi.RMIConnectionImpl;
 import javax.swing.table.TableRowSorter;
 import java.util.Arrays;
 
@@ -68,7 +69,7 @@ public class MonotonicArray {
     }
 
     /**
-     * 合并两个有序数组
+     * LeetCode88 合并两个有序数组
      * 从后往前遍历
      * @param arr1 数组1 ， 大小 m + n
      * @param m 元素个数
@@ -79,14 +80,14 @@ public class MonotonicArray {
         int i = m + n - 1;
         m = m - 1;
         n = n - 1;
-        while (m >= 0 && n >= 0) {
+        while (m >= 0 && n >= 0) { // 注意要两个都大于0
             if (arr1[m] >= arr2[n]) {
                 arr1[i--] = arr1[m--];
             }else {
                 arr1[i--] = arr2[n--];
             }
         }
-        while (n != -1) {
+        while (n != -1) { //注意其中一个数组还有剩余的情况
             arr1[i--] = arr2[n--];
         }
         System.out.println(Arrays.toString(arr1));
@@ -97,6 +98,37 @@ public class MonotonicArray {
         int[] arr1 = {1,3,5,7,8,12,0,0,0};
         int[] arr2 = {-1, 0, 1};
         mergerArray(arr1, 6, arr2, 3);
+    }
+
+    /**
+     * 如leetcode35：给定⼀个排序数组和⼀个⽬标值，在数组中找到⽬标值，并返回其索引。如果⽬标值不存在于数组
+     * 中，返回它将会被按顺序插⼊的位置。
+     * 使用二分查找法解决问题
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int searchInsert(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        int middle = low + ((high - low) >> 1);
+        while (low <= high) {
+            if (nums[middle] < target) {
+                low = middle + 1;
+            } else if (target < nums[middle]) {
+                high = middle - 1;
+            }else {
+                return middle;
+            }
+            middle = low + ((high - low) >> 1);
+        }
+        return middle + 1;
+    }
+
+    @Test
+    public void testSearchInsert() {
+        int[] a = {1, 3, 5, 7, 9};
+        System.out.println(searchInsert(a, 3));
     }
 
 }
