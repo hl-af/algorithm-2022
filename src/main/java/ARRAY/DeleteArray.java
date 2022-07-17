1a277efc08dab7d8a075209a6ffe85a369849388
huanglei1 <huanglei1@joyy.com>
2022-07-17 13:34:55 +0800

    LeetCode 26删除数重复元素

--- src/main/java/ARRAY/DeleteArray.java
+++ src/main/java/ARRAY/DeleteArray.java
@@ -8,6 +8,30 @@ import java.util.Arrays;
 public class DeleteArray {
 
     /**
+     * LeetCode26 删除有序数组的重复项
+     * @param nums
+     * @return
+     */
+    public int removeDuplicates(int[] nums) {
+        int slow = 0;
+        int fast = 1;
+        for (; fast <= nums.length - 1; fast++) {
+            if (nums[slow] != nums[fast]) {
+                slow++;
+                nums[slow] = nums[fast];
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+        return slow;
+    }
+
+    @Test
+    public void testRemoveDuplicates() {
+        int[] a = {1, 3, 4, 5, 6, 7, 7, 8};
+        System.out.println(removeDuplicates(a));
+    }
+
+    /**
      * LeetCode27 原地移除数组中为val的元素，数组无序，不用管新数组长度后面的元素
      * 快慢指针
      * 这个方法不太好理解和记住，还是用对撞指针来做吧
@@ -18,6 +42,7 @@ public class DeleteArray {
     public int removeElement(int[] arr,int val) {
         int fast = 0;
         int slow = 0;
+        // fast指向元素，如果遇到目标元素，slow停下来， 如果遇到非目标元素，交换slow 和fast 指向元素的值
         for (; fast < arr.length; fast++) {
             if (arr[fast] != val) {
                 arr[slow] = arr[fast];
