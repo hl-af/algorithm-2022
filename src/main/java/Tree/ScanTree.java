a5a940f5cefb9219545e735038352d30190353f2
huanglei1 <huanglei1@joyy.com>
2022-07-19 20:55:25 +0800

    Leetcode 144 145 99

--- src/main/java/Tree/ScanTree.java
+++ src/main/java/Tree/ScanTree.java
@@ -1,15 +1,18 @@
 package Tree;
 
+import ListNodeUtils.ListNode;
+import ListNodeUtils.ListUtils;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class ScanTree {
 
-
+    /**
+     * Leetcode 144迭代法实现前序遍历
+     * @param root
+     * @return
+     */
     public List<Integer> preOrderScan(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         Deque<TreeNode> stack = new LinkedList<>();
@@ -31,7 +34,7 @@ public class ScanTree {
         TreeUtils.printTree(res);
     }
     /**
-     * 迭代法实现中序遍历
+     * LeetCode94 迭代法实现中序遍历
      * @param root
      * @return
      */
@@ -52,11 +55,38 @@ public class ScanTree {
         }
         return res;
     }
-
     @Test
     public void testInorderscan() {
         TreeUtils.printTree(inorderscan(TreeUtils.getTreeDemo()));
     }
+    /**
+     * LeetCode145后序遍历
+     *  使用反转链表法来做
+     * @param root
+     * @return
+     */
+    public List<Integer> postOrderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                res.add(root.val);
+                root = root.left;
+            }
+            root = stack.pop();
+            root = root.right;
+        }
+        Collections.reverse(res);
+        return res;
+    }
+
+    @Test
+    public void testPostOrderTraversal() {
+        List<Integer> res = postOrderTraversal(TreeUtils.getTreeDemo());
+        TreeUtils.printTree(res);
+    }
+
 
     //TODO 迭代法实现后续遍历
 }
