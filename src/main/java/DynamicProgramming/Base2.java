888d16c0cdd5080ed3d7c1e336e8fdb8ccb39497
huanglei1 <huanglei1@joyy.com>
2022-10-13 19:33:14 +0800

    最长递增子序列

---
+++ src/main/java/DynamicProgramming/Base2.java
@@ -0,0 +1,63 @@
+package DynamicProgramming;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class Base2 {
+
+    /**
+     * LeetCode322.给你⼀个整数数组 coins ，表示不同⾯额的硬币，以及⼀个整数 amount ，表示总⾦额。计算并返
+     * 回可以凑成总⾦额所需的最少的硬币个数 。如果没有任何⼀种硬币组合能组成总⾦额，返回 -1 。你可以认为每种
+     * 硬币的数量是⽆限的。
+     * @param coins
+     * @param amount
+     * @return
+     */
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1]; // dp[] 代表硬币数， []代表金额
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int currentAmount = 0; currentAmount <= amount; currentAmount++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (currentAmount - coins[j] >= 0 &&
+                dp[currentAmount] > dp[currentAmount - coins[j]] + 1) {
+                    dp[currentAmount] = dp[currentAmount - coins[j]] + 1;
+                }
+            }
+        }
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
+
+    @Test
+    public void testCoinChange() {
+        int[] coins = {1, 2, 5};
+        System.out.println(coinChange(coins, 1));
+    }
+
+    /**
+     *  LeetCode674.给定⼀个未经排序的整数数组，找到最⻓且连续递增的⼦序列，并返回该序列的⻓度。
+     * @param A
+     * @return
+     */
+    public int findLengthOfLCIS(int[] A){
+        int[] dp = new int[A.length];
+        Arrays.fill(dp, 1);
+        int result = 0;
+        for (int i = 1; i < A.length; i++) {
+            if (A[i - 1] < A[i]) {
+                dp[i] = dp[i - 1] + 1;
+            }
+            if (result < dp[i]) {
+                result = dp[i];
+            }
+        }
+        return result;
+    }
+
+    @Test
+    public void testFindLengthOfLCIS() {
+        int[] A = {1,3,5,4,7,8,9,10,11,12};
+        System.out.println(findLengthOfLCIS(A));
+    }
+}
