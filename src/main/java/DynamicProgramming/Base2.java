a82c607824efeeceeb7e489f613f04059b40b798
huanglei1 <huanglei1@joyy.com>
2022-10-14 17:27:06 +0800

    最小平方数

--- src/main/java/DynamicProgramming/Base2.java
+++ src/main/java/DynamicProgramming/Base2.java
@@ -2,7 +2,9 @@ package DynamicProgramming;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Base2 {
 
@@ -59,5 +61,77 @@ public class Base2 {
     public void testFindLengthOfLCIS() {
         int[] A = {1,3,5,4,7,8,9,10,11,12};
         System.out.println(findLengthOfLCIS(A));
+    }
+
+
+
+
+    /**
+     * LeetCode300.给你⼀个整数数组 nums ，找到其中最⻓严格递增⼦序列的⻓度。
+     * @param A
+     * @return
+     */
+    public int lengthOfLIS(int[] A) {
+        int[] dp = new int[A.length];
+        int result = Integer.MIN_VALUE;
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < A.length; i++) {
+            for (int j = 1; j < i; j++) {
+                if (A[j] < A[i] && dp[i] < (dp[j] + 1)) {
+                    dp[i] = dp[j] + 1;
+                }
+                if (result < dp[i]) {
+                    result = dp[i];
+                }
+            }
+        }
+        return result;
+    }
+
+    @Test
+    public void testLengthOfLIS() {
+        int[] A = {10, 9, 2, 5, 3, 7, 101, 1};
+        System.out.println(lengthOfLIS(A));
+    }
+
+    /**
+     * LeetCode279.给你⼀个整数 n ，返回和为n的完全平⽅数的最少数量。
+     * 完全平⽅数 是⼀个整数，其值等于另⼀个整数的平⽅；换句话说，其值等于⼀个整数⾃乘的积。例如，1、4、9 和16 都是完全平⽅数，⽽ 3 和 11 不是。
+      * @param n
+     * @return
+     */
+    public int numSquares(int n) {
+        List<Integer> squarNum = new ArrayList<>();
+        int[] dp = new int[n + 1]; //dp[] 是最少数量，n是目标数
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 1; i <= n ; i++) { //目标数
+            if (isSquarNum(i)) {
+                squarNum.add(i);
+                dp[i] = 1;
+            }
+            for (int j = 0; j < squarNum.size(); j++) {
+                if ( i - squarNum.get(j) >= 0 && dp[i] > dp[i - squarNum.get(j)] + 1){
+                    dp[i] = dp[i - squarNum.get(j)] + 1;
+                }
+            }
+        }
+        return dp[n];
+    }
+
+    public boolean isSquarNum(int n) {
+        Double a = Math.sqrt(n);
+        int b = a.intValue();
+        if (b * b != n) {
+            return false;
+        }else {
+            return true;
+        }
+    }
+
+    @Test
+    public void testNumSquares() {
+        System.out.println(isSquarNum(11));
+        System.out.println(numSquares(17));
     }
 }
