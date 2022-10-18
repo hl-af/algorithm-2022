a22935e1a29500e40b24b3d3e805ecf6e1c24efc
huanglei1 <huanglei1@joyy.com>
2022-10-18 11:00:02 +0800

    LeetCode55青蛙跳问题

--- src/main/java/DynamicProgramming/Base2.java
+++ src/main/java/DynamicProgramming/Base2.java
@@ -119,6 +119,26 @@ public class Base2 {
         return dp[n];
     }
 
+    /**
+     * LeetCode279.给你⼀个整数 n ，返回和为n的完全平⽅数的最少数量。
+     * 完全平⽅数 是⼀个整数，其值等于另⼀个整数的平⽅；换句话说，其值等于⼀个整数⾃乘的积。例如，1、4、9 和16 都是完全平⽅数，⽽ 3 和 11 不是。
+     * @param n
+     * @return
+     */
+    public int numSquaresStandard(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                if (dp[i] > dp[i - j * j]) {
+                    dp[i] = dp[i - j * j] + 1;
+                }
+            }
+        }
+        return dp[n];
+    }
+
     public boolean isSquarNum(int n) {
         Double a = Math.sqrt(n);
         int b = a.intValue();
@@ -131,7 +151,33 @@ public class Base2 {
 
     @Test
     public void testNumSquares() {
-        System.out.println(isSquarNum(11));
         System.out.println(numSquares(17));
+        System.out.println(numSquaresStandard(17));
     }
+
+    /**
+     * LeetCode55.跳跃游戏，给定⼀个⾮负整数数组 nums ，你最初位于数组的 第⼀个下标 。数组中的每个元素代表
+     * 你在该位part置可以跳跃的最⼤⻓度。判断你是否能够到达最后⼀个下标
+     * @param A
+     * @return
+     */
+    public boolean canJump(int[] A) {
+        boolean[] dp = new boolean[A.length]; //默认值是false
+        dp[0] = true;
+        for (int i = 1; i < A.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && A[j] + j >= i) {
+                    dp[i] = true;
+                }
+            }
+        }
+        return dp[A.length - 1];
+    }
+
+    @Test
+    public void testCanJump() {
+        int[] A = {3, 2, 1, 0, 4};
+        System.out.println(canJump(A));
+    }
+
 }
